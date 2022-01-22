915ac3c756652389458580a0a57d1de841a3ef52
Aashutosh Rathi <aashutoshrathi@gmail.com>
2022-01-22 12:29:53 +0530

    yes yes: added sheetDB to access the data from domain

---
+++ index.js
@@ -0,0 +1,22 @@
+// Make url allows you to debug by adding query param ?url=http://activity.domain.com
+const params = (new URL(document.location)).searchParams;
+const url = params.get("url") ?? location.hostname;
+
+const domain = url.split('.').splice(-2).join('.');
+const subdomain = url.split(`.${domain}`)[0];
+
+// Sheet Name - Domain, activity - Subdomain
+console.log({ domain, subdomain });
+
+SheetDB.read('https://sheetdb.io/api/v1/qke75vli3rita', {
+    sheet: domain,
+    search: {
+        activity: subdomain
+    }
+}).then(function (result) {
+    console.log(result[0]);
+    const status = result?.[0]?.['doingIt?'] ?? 'No idea bro';
+    document.getElementById('status').innerText = status + '!';
+}, function (error) {
+    console.log(error);
+});
